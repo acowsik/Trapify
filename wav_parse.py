746eeb7868ff0d9b599e12ed68648ce1b5623670
williamjiao <william.jiao@gmail.com>
2017-11-11 17:42:21 -0500

    added object functionality to parser

--- wav_parse.py
+++ wav_parse.py
@@ -1,39 +1,50 @@
 import wave as wv
 import struct
 
+class wavFile:
+	stream = None
 
-def parse_wav(wave_file):
-	stream = wv.open(wave_file,"rb")
 
-	num_channels = stream.getnchannels()
-	sample_rate = stream.getframerate()
-	sample_width = stream.getsampwidth()
-	num_frames = stream.getnframes()
+	def __init__(self, fileName):
+		self.stream = wv.open(fileName, "rb")
 
-	raw_data = stream.readframes( num_frames ) # Returns byte data
-	stream.close()
+	def stream(nSamples):
+		num_channels = stream.getnchannels()
+		sample_rate = stream.getframerate()
+		sample_width = stream.getsampwidth()
+		num_frames = stream.getnframes()
+		raw_data = stream.readframes(nSamples)
 
-	total_samples = num_frames * num_channels
+		try:
+			if sample_width == 1: 
+				fmt = "%iB" % nSamples # read unsigned chars
+			elif sample_width == 2:
+				fmt = "%ih" % (nSamples) # read signed 2 byte shorts
+			else:
+				raise ValueError("Only supports 8 and 16 bit audio formats.")
+		except:
+			return None
 
-	if sample_width == 1: 
-		fmt = "%iB" % total_samples # read unsigned chars
-	elif sample_width == 2:
-		fmt = "%ih" % (total_samples) # read signed 2 byte shorts
-	else:
-		raise ValueError("Only supports 8 and 16 bit audio formats.")
+		float_data = struct.unpack(fmt, raw_data)
+		del raw_data # Keep memory tidy (who knows how big it might be)
 
-	float_data = struct.unpack(fmt, raw_data)
-	del raw_data # Keep memory tidy (who knows how big it might be)
+		channels = [ [] for time in range(num_channels) ]
 
-	channels = [ [] for time in range(num_channels) ]
-
-	for index, value in enumerate(float_data):
-		bucket = index % num_channels
-		channels[bucket].append(value)
-		
-	first_channel = float_data[::num_channels]
-	floatChannel = map(float, first_channel)
+		for index, value in enumerate(float_data):
+			bucket = index % num_channels
+			channels[bucket].append(value)
+		first_channel = float_data[::num_channels]
+		floatChannel = map(float, first_channel)
 	return floatChannel
+
+	def rewind():
+		stream.rewind()
+
+	def getLength():
+		return stream.getnframes()
+
+def get_wavfile(fileName):
+	return waveFile(fileName)
 	
 
 def construct_wav(channels, name):
